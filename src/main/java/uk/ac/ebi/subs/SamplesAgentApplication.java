a10352192845f4df2653a6ecd4dc8f5be5600554
Neil Goodgame <neilg@ebi.ac.uk>
2017-07-18 16:00:12 +0100

    Added application pid to samples agent

--- src/main/java/uk/ac/ebi/subs/SamplesAgentApplication.java
+++ src/main/java/uk/ac/ebi/subs/SamplesAgentApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SamplesAgentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SamplesAgentApplication.class, args);
+        SpringApplication springApplication = new SpringApplication( SamplesAgentApplication.class);
+        ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
+        springApplication.addListeners( applicationPidFileWriter );
+        springApplication.run(args);
     }
 }
