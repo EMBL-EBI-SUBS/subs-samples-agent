408e836e043bb81a863cefefd080a01ed2841f7f
Flavia Penim <flaviapenim@gmail.com>
2017-09-26 10:50:27 +0100

    Configure externalCiTest task and remove unnecessary mongo from travis config file.

--- src/test/java/uk/ac/ebi/subs/agent/services/IntegrityServiceTest.java
+++ src/test/java/uk/ac/ebi/subs/agent/services/IntegrityServiceTest.java
@@ -2,11 +2,13 @@ package uk.ac.ebi.subs.agent.services;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.subs.agent.utils.BioSamplesDependentTest;
 import uk.ac.ebi.subs.agent.utils.TestUtils;
 import uk.ac.ebi.subs.data.component.Team;
 import uk.ac.ebi.subs.data.submittable.Sample;
@@ -16,6 +18,7 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
+@Category(BioSamplesDependentTest.class)
 public class IntegrityServiceTest {
 
     @Autowired
